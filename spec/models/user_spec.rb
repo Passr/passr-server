64418de641de49975c775ea3d279386598cf7472
Brady Love <love.brady@gmail.com>
2015-07-03 12:42:21 -0700

    Require password confirmation if password is present

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+describe User, type: :model do
+  describe 'password confirmation' do
+    let(:user) { User.new(email: 'john.doe@example.com', password: 'passw0rd') }
+
+    it 'is invalid without password confirmation' do
+      expect(user).to be_invalid
+    end
+
+    it 'is valid with matching password confirmation' do
+      user.password_confirmation = 'passw0rd'
+
+      expect(user).to be_valid
+    end
+
+    it 'is invalid with non matching password confirmation' do
+      user.password_confirmation = 'wrongPassword'
+
+      expect(user).to be_invalid
+    end
+  end
 end
