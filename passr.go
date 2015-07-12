f1d4951343b44d94935de9b836173bb1b7a7f01a
ant <anth1y@gmail.com>
2015-07-12 13:50:22 -0700

    cleaning up the server from my stupid hacks

--- passr.go
+++ passr.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	passwrd := []byte("SomeReallyLong Awful String")
-
-	// Hashing the password givn witht he cost of 10
-	hashedPassword, err := bcrypt.GenerateFromPassword(passwrd, 10)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(hashedPassword))
-
-	// Compare the password with the hash
-	err = bcrypt.CompareHashAndPassword(hashedPassword, passwrd)
-	fmt.Println(err) // nil means  it is a match
 
 	r := gin.Default()
 
